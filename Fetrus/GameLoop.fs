7a3f1834be95f35e5d7ecad38a054a17e00c8371
3tilley <3tilley@googlemail.com>
2017-12-25 14:24:13 +0000

    With key controls

---
+++ Fetrus/GameLoop.fs
@@ -0,0 +1,30 @@
+﻿module GameLoop
+
+let directions = [System.ConsoleKey.LeftArrow; System.ConsoleKey.DownArrow; System.ConsoleKey.RightArrow]
+
+let rec gameLoop state lastTick timeBetweenTicks lastRender timeBetweenRenders =
+    
+    let nextTick = lastTick + timeBetweenTicks
+    let now = System.DateTime.UtcNow
+    if now > nextTick then
+
+        printf "Ticked"
+        gameLoop (Model.tick state) (lastTick + timeBetweenTicks) timeBetweenTicks lastRender timeBetweenRenders
+    let nextRender = lastRender + timeBetweenRenders
+    let now = System.DateTime.UtcNow
+    if now > nextRender then
+        System.Console.SetCursorPosition(0,0)
+        System.Console.Write(state.ToString())
+        printf "Rendered"
+        gameLoop state lastTick timeBetweenTicks (lastRender + timeBetweenRenders) timeBetweenRenders
+    if System.Console.KeyAvailable then
+        let k = System.Console.ReadKey(true)
+        if directions |> List.contains k.Key then
+            let dir =
+                match k.Key with
+                | System.ConsoleKey.LeftArrow -> Model.Direction.Left
+                | System.ConsoleKey.RightArrow -> Model.Direction.Right
+                | System.ConsoleKey.DownArrow -> Model.Direction.Down
+                | _ -> failwith "Key unrecognised"
+            gameLoop (Model.move state (Model.Translate dir)) lastTick timeBetweenTicks lastRender timeBetweenRenders
+    gameLoop state lastTick timeBetweenTicks lastRender timeBetweenRenders
--- Fetrus/Program.fs
+++ Fetrus/Program.fs
@@ -1,7 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open Model
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    System.Console.CursorVisible <- false
+    let constantBlockGen cols =
+        { Coords = [(0, 0); (0, 1); (1, 0); (1, 1)]; BlockType = Square }
+
+    let start = Model.State(Array2D.create 10 20 false, constantBlockGen 10, constantBlockGen)
+    let now = System.DateTime.UtcNow
+    let renderTime = System.TimeSpan.FromSeconds(1.0 / 10.0)
+    let tickTime = System.TimeSpan.FromSeconds(1.0)
+    GameLoop.gameLoop start now tickTime now renderTime
     0 // return an integer exit code
